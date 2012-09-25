9ac195798ca55ecef2ac4d2e3f40ed204f8089e6
Matthew Radke <themoderngrad@gmail.com>
2012-09-24 20:41:51 -0400

    Initial Commit

---
+++ MovieApplicationTests/MovieApplicationTests.h
@@ -0,0 +1,13 @@
+//
+//  MovieApplicationTests.h
+//  MovieApplicationTests
+//
+//  Created by matthew radke on 9/24/12.
+//  Copyright (c) 2012 matthew radke. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MovieApplicationTests : SenTestCase
+
+@end
---
+++ MovieApplicationTests/MovieApplicationTests.m
@@ -0,0 +1,32 @@
+//
+//  MovieApplicationTests.m
+//  MovieApplicationTests
+//
+//  Created by matthew radke on 9/24/12.
+//  Copyright (c) 2012 matthew radke. All rights reserved.
+//
+
+#import "MovieApplicationTests.h"
+
+@implementation MovieApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MovieApplicationTests");
+}
+
+@end
